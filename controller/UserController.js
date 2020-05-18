cbe8c61d9e8eeb21f6a36cd052c1e904f34e8622
bh-fst02 <bh.fst02@gmail.com>
2020-05-18 16:56:42 +0200

    #FST2-37 create mailerservice

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,10 +1,12 @@
-const User = require('../model/user.js');
+const User = require('../model/user');
+const MailerService = require('../service/MailerService');
 
 const create = async (userData) => {
   if (userData.password !== userData.passwordRepeat) {
     throw new Error('Passwords do not match');
   }
   await User.create(userData);
+  MailerService.send(userData.email, 'testsubject', 'testtext');
 };
 
 module.exports = {
---
+++ service/MailerService.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'csandor444@gmail.com',
+    pass: 'braininghub',
+  },
+});
+
+module.exports = {
+  send(to, subject, text) {
+    const mailOptions = {
+      from: 'csandor444@gmail.com',
+      to,
+      subject,
+      text,
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(`Email sent: ${info.response}`);
+      }
+    });
+  },
+};
