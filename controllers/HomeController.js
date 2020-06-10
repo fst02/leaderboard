a87e71b4696980d6d5ca256a81ed5e21ba4d3f5f
bh-fst02 <bh.fst02@gmail.com>
2020-06-10 14:29:39 +0200

    FST2-52 create pagination in scoreboard

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,8 +1,13 @@
 const ScoreboardService = require('../services/ScoreboardService');
 
 const scoreboard = (req, res) => {
-  const { order, game, orderByColumn } = req.query;
-  ScoreboardService.select(orderByColumn, order, game).then((data) => {
+  const {
+    order,
+    game,
+    orderByColumn,
+  } = req.query;
+  const offset = req.query.offset || '0';
+  ScoreboardService.select(orderByColumn, order, game, offset).then((data) => {
     const loggedIn = req.session.loggedIn === true;
     res.render('home/scoreboard', {
       title: 'Leaderboard',
@@ -10,6 +15,8 @@ const scoreboard = (req, res) => {
       loggedIn,
       game: game || 'All',
       nickname: req.session.nickname,
+      previousOffset: Math.max(0, parseInt(offset, 10) - 5),
+      nextOffset: parseInt(offset, 10) + 5,
     });
   });
 };
--- services/ScoreboardService.js
+++ services/ScoreboardService.js
@@ -2,19 +2,20 @@ const Scoreboard = require('../models/Scoreboard');
 const User = require('../models/User');
 const Game = require('../models/Game');
 
-const select = async (orderColumn = 'topScore', orderDirection = 'DESC', game = 'All') => {
+const select = async (orderColumn = 'topScore', orderDirection = 'DESC', game = 'All', offset = '0') => {
   const whereStatement = {};
   if (game !== 'All') {
     whereStatement.game = game;
   }
   const results = await Scoreboard.findAll({
+    limit: 5,
+    offset: parseInt(offset, 10),
     where: whereStatement,
     order: [[orderColumn, orderDirection]],
     include: [User, Game],
   });
   const exportData = [];
   results.forEach((result) => {
-    console.log(result);
     exportData.push({
       id: result.id,
       name: result.user.nickname,
